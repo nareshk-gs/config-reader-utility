d4033c7593f1e1972f0cadde9062ebaa66c1b446
Naresh Gourishetty <naresh.gourishetty@kroger.com>
2018-10-15 14:07:03 -0400

    Added support to YML files

--- src/main/java/com/configreader/utils/BaseConfig.java
+++ src/main/java/com/configreader/utils/BaseConfig.java
@@ -3,10 +3,13 @@ package com.configreader.utils;
 import com.configreader.ConfigReader;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseConfig implements ConfigReader {
 
     public static final String BASIC = "project";
+    public Map<String, String> properties;
 
     public String getPath() {
         return System.getProperty("user.dir") + File.separator
@@ -14,13 +17,16 @@ public class BaseConfig implements ConfigReader {
                 + "resources" + File.separator;
     }
 
-    public Object readConfig(String env) {
-        return new Object();
+    public Map<String, String> readConfig(String env) {
+        return new HashMap<>();
     }
-
-
-    public Object getConfig() {
-        return new Object();
+    
+    public void mergeConfig(Map<String, String> props) {
+        props.forEach(properties::putIfAbsent);
+    }
+    
+    public Map<String, String> getConfig() {
+        return new HashMap<>();
     }
 
     public String makeKey(String... parts) {
--- src/test/java/com/configreader/utils/PropertyConfigTest.java
+++ src/test/java/com/configreader/utils/PropertyConfigTest.java
@@ -2,6 +2,7 @@ package com.configreader.utils;
 
 import org.testng.annotations.Test;
 
+import java.util.Map;
 import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,16 +13,16 @@ public class PropertyConfigTest {
 
     @Test
     public void loadGlobalProps() {
-        Properties properties = new PropertyConfig().getConfig();
+        Map<String, String> properties = new PropertyConfig().getConfig();
         assertThat(3, is(equalTo(properties.size())));
-        assertThat("global", is(equalTo(properties.getProperty("property"))));
+        assertThat("global", is(equalTo(properties.get("property"))));
     }
 
     @Test
     public void loadMergedProps() {
-        Properties properties = new PropertyConfig("specific").getConfig();
+        Map<String, String> properties = new PropertyConfig("specific").getConfig();
         assertThat(5, is(equalTo(properties.size())));
-        assertThat("specific", is(equalTo(properties.getProperty("property"))));
+        assertThat("specific", is(equalTo(properties.get("property"))));
     }
 
 
